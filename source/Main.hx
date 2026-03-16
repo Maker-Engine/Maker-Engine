78ac63dee533f8cd19d143184f3a11da6ad3c3b5
Brenninho <157061485+Brenninho123@users.noreply.github.com>
2026-03-16 11:52:51 -0300

    Atualizar o Main.hx

--- source/Main.hx
+++ source/Main.hx
@@ -2,55 +2,46 @@ package;
 
 import openfl.display.Sprite;
 import openfl.events.Event;
-import openfl.Lib;
 import openfl.display.StageAlign;
 import openfl.display.StageScaleMode;
-
-import maker.core.Engine;
-import maker.core.Scene;
+import engine.Engine;
 
 class Main extends Sprite
 {
-    public static var instance:Main;
-    public static var engine:Engine;
+	public static var instance:Main;
+	public static var engine:Engine;
 
-    public function new()
-    {
-        super();
-        instance = this;
+	public function new()
+	{
+		super();
+		instance = this;
 
-        if (stage != null)
-            init();
-        else
-            addEventListener(Event.ADDED_TO_STAGE, init);
-    }
+		if (stage != null)
+			init();
+		else
+			addEventListener(Event.ADDED_TO_STAGE, init);
+	}
 
-    private function init(?e:Event):Void
-    {
-        removeEventListener(Event.ADDED_TO_STAGE, init);
+	private function init(?e:Event):Void
+	{
+		removeEventListener(Event.ADDED_TO_STAGE, init);
 
-        setupStage();
-        startEngine();
-    }
+		setupStage();
+		startEngine();
+	}
 
-    private function setupStage():Void
-    {
-        stage.align = StageAlign.TOP_LEFT;
-        stage.scaleMode = StageScaleMode.NO_SCALE;
-        stage.frameRate = 60;
-    }
+	private function setupStage():Void
+	{
+		stage.align = StageAlign.TOP_LEFT;
+		stage.scaleMode = StageScaleMode.NO_SCALE;
+		stage.frameRate = 60;
+	}
 
-    private function startEngine():Void
-    {
-        engine = new Engine();
+	private function startEngine():Void
+	{
+		engine = new Engine();
+		addChild(engine);
 
-        // Load first scene
-        var startScene = new Scene();
-        engine.setScene(startScene);
-
-        addChild(engine);
-
-        trace("Maker Engine started successfully.");
-    }
-
+		trace("Maker Engine initialized.");
+	}
 }
